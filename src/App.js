9ccfe4a943e328e0dcb30f5cab6396e768efb3ef
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2022-08-12 13:57:08 -0700

    Reset to React App basic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react'
-import MyClassComponent from './MyClassComponent';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    const sayHi = (name) => `Hello ${name}!!!!!!!!`;    
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{sayHi("Bhargav")}</p>
-          <MyClassComponent></MyClassComponent>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
