6231f3d296037646d506fbe622a87a09e9e529c8
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2022-08-12 14:30:07 -0700

    Passing Props String and Object(array)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ParentComp/>
       </header>
     </div>
   );
+}
+
+class ParentComp extends Component {  
+  render() {
+    const tomsFriends = ['Nikhil','Tommy','Alan','Thomas'];
+    return (
+      <div>
+        <h2>I am parent component</h2>
+        <Kid kidName="Tom" friends={tomsFriends}/>
+      </div>
+    )
+  }
+}
+
+
+class Kid extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h3>I am child comp: <strong>{this.props.kidName}</strong></h3>
+        <p>
+          {
+            this.props.friends.map((friend, i) => {
+              return friend+"-";
+            })
+          }
+          </p>
+      </div>
+    )
+  }
 }
 
 export default App;
